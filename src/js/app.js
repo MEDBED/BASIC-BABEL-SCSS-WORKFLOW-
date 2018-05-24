872f820c6efdeb6c89c132648d31c39dab38dd1b
Mohamed BOUHAMED <med.bouhamed@live.fr>
2018-05-24 10:37:52 +0200

    Project structure before installing dependency

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+const outputParagraph = document.querySelector('#outputParagraph');
+
+const outputRandomInt = () => {
+    outputParagraph.textContent = RandomGenerator.RandomInteger();
+};
+
+const outputRandomRange = () =>{
+    outputParagraph.textContent = RandomGenerator.randomRange(1,500);
+};
+
+const buttonRandomInt = document.querySelector('#randomInt');
+const buttonRandomRange = document.querySelector('#randomRange');
+
+buttonRandomInt.addEventListener('click',outputRandomInt);
+buttonRandomRange.addEventListener('click',outputRandomRange);
